bfdfaf58c1172dfb8240a419b47fef1f2cdfb9dc
MassMatsu <masa10matsukawa@gmail.com>
2021-05-12 14:55:50 +0900

    fixed the usability issues setTimeout and another useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-//import Accordion from './components/Accordion';
-//import {items} from './utils/data'
 import Search from './components/Search'
 
 class App extends React.Component {
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,34 +1,76 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-import wikipedia from '../api/wikipedia'
-import axios from 'axios'
+import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('')
+  const [term, setTerm] = useState('programming');
+  const [debouncedTerm, setDebouncedTerm] = useState(term)
 
-  const onFormSubmit = (e) => {
-    e.preventDefault()
-    console.log(term)
-    onTermSubmit(term)
-  }
+  const [results, setResults] = useState([]);
 
-  const onTermSubmit = async (term) => {
-    const response = await axios.get(
-      `www.https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&search=${term}`);
-    console.log(response);
-  }
+  console.log(results);
 
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term)
+    }, 3000)
+    return () => {
+      clearTimeout(timerId)
+    }
+  }, [term])
+
+  useEffect(() => {
+    fetchData(debouncedTerm)
+
+  }, [debouncedTerm]);
+
+  const fetchData = async (term) => {
+    const { data } = await axios.get(`https://en.wikipedia.org/w/api.php`, {
+      params: {
+        action: 'query',
+        list: 'search',
+        origin: '*',
+        format: 'json',
+        srsearch: term,
+      },
+    });
+    setResults(data.query.search);
+  };
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div className='item' key={result.pageid}>
+        <div className='right floated content'>
+          <a
+            className='ui button'
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className='content'>
+          <div className='header'>{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
   return (
     <>
       <div className='ui segment'>
-        <form className='ui form' onSubmit={onFormSubmit}>
+        <div className='ui form'>
           <div className='field'>
             <label>Search</label>
-            <input type='text' value={term} onChange={(e) => setTerm(e.target.value)} />
+            <input
+              className='input'
+              type='text'
+              value={term}
+              onChange={(e) => setTerm(e.target.value)}
+            />
           </div>
-        </form>
+        </div>
+        <div className='ui celled list'>{renderedResults}</div>
       </div>
-      <div className="ui segment"></div>
     </>
   );
 };
